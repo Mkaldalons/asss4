232367bf004d68b28f14abc2471e53066d59e9e3
Maria Ros <mrk9@hi.is>
2023-02-07 11:48:32 +0000

    Bætti við long breytu

--- src/main/java/is/hi/mrk9/hbv202g/ass4/App.java
+++ src/main/java/is/hi/mrk9/hbv202g/ass4/App.java
@@ -1,4 +1,5 @@
 package is.hi.mrk9.hbv202g.ass4;
+import org.apache.commons.numbers.primes.*;
 
 /**
  * Hello world!
@@ -8,6 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        long tala = 123456789;
         System.out.println( "Hello World!" );
     }
 }
