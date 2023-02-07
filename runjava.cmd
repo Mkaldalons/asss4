ab07e4736e2838f8e1bef8c14190084a07dfb3ce
Maria Ros <mrk9@hi.is>
2023-02-07 12:49:38 +0000

    Dependency bætt við í target

--- runjava.cmd
+++ runjava.cmd
@@ -1,1 +1,1 @@
-java -cp target\ass4-1.0-SNAPSHOT-tests.jar is.hi.mrk9.hbv202g.ass4.App
+java -cp target\ass4-1.0-SNAPSHOT.jar;target/dependency/commons-numbers-primes-1.1.jar is.hi.mrk9.hbv202g.ass4.App
